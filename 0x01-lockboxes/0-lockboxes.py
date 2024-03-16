a6f743cc38c4935631f81c62400ed7e14b8cdfc9
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-16 19:01:14 +0300

    Added method that determines if all the boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    # Create a set to keep track of the boxes that have been visited
+    visited = set()
+    # Add the first box to the visited set
+    visited.add(0)
+    # Create a stack to store the boxes that need to be visited
+    stack = [0]
+    
+    # Iterate through the stack until it is empty
+    while stack:
+        # Pop the top box from the stack
+        current_box = stack.pop()
+        # Iterate through the keys in the current box
+        for key in boxes[current_box]:
+            # If the key opens a new box and the box has not been visited yet
+            if key < len(boxes) and key not in visited:
+                # Add the box to the visited set
+                visited.add(key)
+                # Add the box to the stack to be visited later
+                stack.append(key)
+    
+    # If all boxes have been visited, return True
+    if len(visited) == len(boxes):
+        return True
+    # Otherwise, return False
+    else:
+        return False
