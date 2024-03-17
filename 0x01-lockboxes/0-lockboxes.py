2b668e1ac0adb0851f5c1b170bf890ab210e92f1
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-17 19:23:44 +0300

    Added method that determines if all the boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,21 +4,8 @@ It contains a function called canUnlockAll that determines
 whether all the boxes in a given list can be unlocked.
 
 The canUnlockAll function takes a list of boxes as
-input and returns True if all the boxes can be unlocked, and False otherwise.
-
-The algorithm used in the canUnlockAll function is as follows:
-1. Create a set called visited to keep track of the boxes that have been visited.
-2. Add the first box (index 0) to the visited set.
-3. Create a stack to store the boxes that need to be visited,
-and initialize it with the first box.
-4. Iterate through the stack until it is empty:
-    - Pop the top box from the stack.
-    - Iterate through the keys in the current box.
-    - If the key opens a new box (index within the range of
-    the boxes list) and the box has not been visited yet,
-    add the box to the visited set and push it onto the stack.
-5. After the iteration, if the number of visited boxes is equal
-to the total number of boxes, return True. Otherwise, return False.
+input and returns True if all the boxes can be unlocked,
+and False otherwise.
 """
 
 
@@ -46,7 +33,8 @@ def canUnlockAll(boxes):
         current_box = stack.pop()
         # Iterate through the keys in the current box
         for key in boxes[current_box]:
-            # If the key opens a new box and the box has not been visited yet
+            # If the key opens a new box and the box has
+            # not been visited yet, add it to the stack
             if key < len(boxes) and key not in visited:
                 # Add the box to the visited set
                 visited.add(key)
