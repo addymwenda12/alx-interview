3ec0d1c83a52a07c5b469ac7dd3bac6e99c80232
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-24 18:16:57 +0000

    Add solution for calculating fewest operations to reach n H characters

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import random
+"""
+Module for calculating the fewest number of operations needed to
+result in exactly n H characters in the file.
+"""
+
+
+def min_operations(n):
+    """
+    Calculate the minimum number of operations needed to
+    get n H characters in the file.
+
+    :param n: int, the number of H characters to get in the file
+    :return: int, the minimum number of operations needed to get n H characters
+    """
+    if n == 0:
+        return 0
+    dp = [0] * (n + 1)
+    for i in range(1, n + 1):
+        if i == 1:
+            dp[i] = 1
+        else:
+            dp[i] = min(dp[i - 1], dp[i - 2] + 1)
+    return dp[n]
