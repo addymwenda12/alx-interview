40fa88598113b973b9d025f91b82602dac8e9ae5
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-03-24 18:39:36 +0000

    Implemented pep8 validation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 """
-Module for calculating the fewest number of operations needed to result in exactly n H characters in the file.
+Module for calculating the fewest number of operations needed
+to result in exactly n H characters in the file.
 """
 
 
 def minOperations(n):
     """
-    Calculate the fewest number of operations needed to result in exactly n H characters in the file.
-    
+    Calculate the fewest number of operations needed to result
+    in exactly n H characters in the file.
     Args:
         n (int): The desired number of H characters.
-        
     Returns:
-        int: The fewest number of operations needed. If n is impossible to achieve, return 0.
+        int: The fewest number of operations needed.
+        If n is impossible to achieve, return 0.
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         if n % divisor == 0:
             n //= divisor
             operations += divisor
         else:
             divisor += 1
-    
+
     return operations
