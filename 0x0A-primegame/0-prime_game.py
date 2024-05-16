0ce9ac7b8b468e05f7d3916e743cf5358819d83a
Addy <123188947+addymwenda12@users.noreply.github.com>
2024-05-16 15:01:43 +0300

    Implement game simulation and winner determination in prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+Prime Game Module
+
+This module contains functions to simulate a game where two players take turns
+to remove a prime number and its multiples from a set of consecutive integers.
+The player who cannot make a move loses the game.
+"""
+
+
+def generate_primes(n):
+    """
+    Generate a list of booleans representing prime numbers up to n.
+
+    Args:
+        n (int): The upper limit for generating prime numbers.
+
+    Returns:
+        list: A list of booleans where the index represents the number and
+              the value at that index represents whether the number is prime.
+    """
+    primes = [False, False] + [True for _ in range(n-1)]
+    for i in range(2, int(n**0.5) + 1):
+        if primes[i]:
+            for j in range(i*i, n + 1, i):
+                primes[j] = False
+    return primes
+
+
+def play_round(n):
+    """
+    Simulate a round of the game with a maximum number n.
+
+    Args:
+        n (int): The maximum number for the round.
+
+    Returns:
+        str: The name of the player who wins the round.
+    """
+    primes = generate_primes(n)
+    player = 0
+    while True:
+        if any(primes):
+            prime = primes.index(True)
+            for i in range(prime, n + 1, prime):
+                primes[i] = False
+            player = 1 - player
+        else:
+            break
+    return 'Maria' if player else 'Ben'
+
+
+def is_winner(x, nums):
+    """
+    Simulate x rounds of the game and determine the overall winner.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list): A list of maximum numbers for each round.
+
+    Returns:
+        str: The name of the player who wins the most rounds,
+        or None if there is a tie.
+    """
+    scores = {'Maria': 0, 'Ben': 0}
+    for n in nums:
+        winner = play_round(n)
+        scores[winner] += 1
+    if scores['Maria'] > scores['Ben']:
+        return 'Maria'
+    elif scores['Maria'] < scores['Ben']:
+        return 'Ben'
+    else:
+        return None
